9214496f0dbd99f10630a60685a140f3098b1aac
Larxmind <juan.sanchez.cortes.al@iespoligonosur.org>
2022-10-31 10:22:47 +0100

    Primer commit, creación modelo Marca y Producto

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=100)
+    unidades = models.IntegerField()
+    precio = models.FloatField()
+    detalles = models.TextField()
+    marca = models.ForeignKey('Marca', on_delete=models.CASCADE)
+
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=100, primary_key=True)
